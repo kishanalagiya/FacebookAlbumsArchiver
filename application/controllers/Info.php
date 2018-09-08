a76a51ce81d5451e0025f086af92bd52aa762c3b
kishan <37392469+kishanalagiya@users.noreply.github.com>
2018-09-08 21:54:00 +0530

    Update Info.php

--- application/controllers/Info.php
+++ application/controllers/Info.php
@@ -2,7 +2,7 @@
 /**
  *  
  *
- * PHP version 5.4
+ * PHP version 7.2
  *
  * @category PHP
  * @package  Master
@@ -10,8 +10,10 @@
  * @license  MIT License
  * @link     https://github.com/kishanalagiya/FacebookAlbumsArchiver
  */
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed'); 
 ob_start();
+ 
+ 
 class Info extends CI_Controller 
 {
         
@@ -20,8 +22,8 @@ class Info extends CI_Controller
 		$this->load->view('info');
 	}
 
-	public function view_photo()
-	{
+	public function view_photo($id=0)
+	{ 
 	    if($this->session->userdata('valid')!=89053)
         {
             redirect('index.php/signin');    
@@ -30,45 +32,39 @@ class Info extends CI_Controller
 	    try{
 		$this->load->library('facebook');
 	
-		$album_id = @$_GET['album_id'];
-		$album_name = @$_GET['album_name'];
-        //$limitmax=PHP_INT_MAX;
-	    //Get access token from session
+		$album_id = $id;
+		 
+	 
 		$access_token = $_SESSION['fb_access_token'];
- 
-		// Get photos of Facebook page album using Facebook Graph API
-    	//$url = "https://graph.facebook.com/v3.1/{$album_id}/photos?fields=source,images,name&limit=100&access_token={$access_token}";
-	    //  $fb->get('/' . $album_id . '/photos?fields=id,source');
-              
+  
         $url ="https://graph.facebook.com/v3.1/".$album_id."/photos?fields=images%2Calbum&access_token={$access_token}";
-        // var_dump($url);
+         
         $pic=file_get_contents($url);
         $pictures=json_decode($pic);
-        //$url1=$url;
-        //var_dump($pictures->data);
+        
          if($pictures->data!=NULL)
          {
-        $page=(array)$pictures->paging;
-        //print_r($page);
-        $fbPhotoData=array();
-        do
-        {
-             foreach($pictures->data as $my)
+             $page=(array)$pictures->paging;
+            
+             $fbPhotoData=array();
+             do
              {
-                 array_push($fbPhotoData,$my->images[0]->source); 
+                  foreach($pictures->data as $my)
+                  {
+                         array_push($fbPhotoData,$my->images[0]->source); 
  
-             }
-             if(array_key_exists("next",$page))
-             {
-                 $url=$page["next"];
-                 $pic=file_get_contents($url);
-                 $pictures=json_decode($pic);
-                 $page=(array)$pictures->paging;
-             }
-             else
-             {
-                 $url='none';       
-             }
+                  }
+                if(array_key_exists("next",$page))
+                 {
+                    $url=$page["next"];
+                    $pic=file_get_contents($url);
+                    $pictures=json_decode($pic);
+                    $page=(array)$pictures->paging;
+                 }
+                 else
+                 {
+                     $url='none';       
+                 }
         
           }while($url!='none');    
 		
@@ -108,61 +104,101 @@ class Info extends CI_Controller
 	   $this->load->library('google');
 	   $this->load->library('zip');
 	   $download=array();
-	 try{
-	   if($uid==1)
-	        $album_id = $_POST['downloadId'];
-	   else if($uid==2)
-	        $album_id = $_POST['moveId'];
-	   else
-	        redirect("index.php/signin");
-       $access_token = $_SESSION['fb_access_token'];
-       $url ="https://graph.facebook.com/v3.1/".$album_id."/photos?fields=images%2Calbum&access_token={$access_token}";
-       $pic=file_get_contents($url);
-       $pictures=json_decode($pic);
-        if($pictures->data!=NULL)
-         {
-       $page=(array)$pictures->paging;
-       
-       do
-       {
-           foreach($pictures->data as $my)
-           { 
-                  
-               $fileContent = file_get_contents($my->images[0]->source);
-               $name=explode("?",$my->images[0]->source);
-               $this->zip->add_data(basename($name[0]), $fileContent);
-           }
-           if(array_key_exists("next",$page))
-            {
-                $url=$page["next"];
-                $pic=file_get_contents($url);
-                $pictures=json_decode($pic);
-                $page=(array)$pictures->paging;
+	   $plist=array();
+	   try
+	   {
+	         if($uid==1)
+	         {
+	              $album_id = $_POST['downloadId'];
+	              $album_name=$_POST['DAlbumName'];
+	             
+	               
+	         }
+	         else if($uid==2)
+	         {
+	             
+	                 $album_id = $_POST['moveId'];
+	                 $album_name=$_POST['AlbumName'];
+	           
+	         }
+	          
+	             
+            $access_token = $_SESSION['fb_access_token'];
+            $url ="https://graph.facebook.com/v3.1/{$album_id}/photos?fields=images%2Calbum&access_token={$access_token}";
+            $pic=file_get_contents($url);
+            $pictures=json_decode($pic);
             
+            if($pictures->data!=NULL)
+            {
+                $page=(array)$pictures->paging;
+                 
+       
+                do
+                {
+                    foreach($pictures->data as $my)
+                    { 
+                      
+                            
+                  
+                        $fileContent = file_get_contents($my->images[0]->source);
+                        $name=explode("?",$my->images[0]->source);
+                         $this->zip->add_data(basename($name[0]), $fileContent);
+                         array_push($plist,$my->images[0]->source);
+                             
+                         
+                    }
+                    if(array_key_exists("next",$page))
+                    {
+                        $url=$page["next"];
+                        $pic=file_get_contents($url);
+                        $pictures=json_decode($pic);
+                        $page=(array)$pictures->paging;
+            
+                    }
+                    else
+                    {
+                        $url='none';       
+                    }
+        
+                }while($url!='none');    
+             
+           
+            $name1=$this->generateRandomString();
+            array_push($download,$name1);   
+            if(!@$_POST['moveId'] || $uid==1)
+            {  
+                 
+                $this->zip->archive(FCPATH.'/uploads/'.$name1.'.zip');
+                $this->zip->clear_data();
+                $album['data']= $download;
+		        $album['album_name']= $album_name;
+	    	    $this->load->view('downloadalbum',$album);
+	    	     
+	    	    
+                 
             }
             else
             {
-                $url='none';       
-            }
-        
-        }while($url!='none');    
-             
-        $name=$this->generateRandomString();
-        array_push($download,$name);           
-        $this->zip->archive(FCPATH.'/uploads/'.$name.'.zip');
-        $this->zip->clear_data();
-        
-        if($uid==2)
-		{
+                if($uid=2 && @$_POST['moveId'] )
+		        {
+		            $_SESSION['google']=$download;
+		            $_SESSION['plist']=$plist;
+		            $_SESSION['aname']=$album_name;
+		            echo '<script>alert("Your Album is uploading.....");</script>'; 
+		             echo '<script>alert("Close the tab in case of taking long time.And Check Your Drive Album Is being Uploading.");</script>'; 
+		              
+		            if($this->session->userdata('code')==NULL)
+		            {
+		                echo '<script>window.location="'.$this->google->loginURL1().'";</script>';
+		            }
+		            else
+		            {
+		                 redirect('index.php/User_authentication/index1?code='.$this->session->userdata('code'));
+		            }
+		        }
 		    
-		    $_SESSION['google']=$download;
-		    redirect($this->google->loginURL());
-		}
-		else
-		{
-		    $album['data']= $download;
-	    	$this->load->view('downloadalbum',$album);
-		}
+            } 
+		    
          }
          else
          {
@@ -172,14 +208,18 @@ class Info extends CI_Controller
               </script>';
               
          }
-       }catch(Exception $e)
+       }
+       catch(Exception $e)
        {
            echo "Error in single download ".$e;
        }
          
         
-             
+         
+        
+            
     }
+	
 	
 	function generateRandomString() 
 	{
@@ -204,6 +244,9 @@ class Info extends CI_Controller
         {
             redirect('index.php/signin');    
         }
+          
+         
+        
         $this->load->library('facebook');
          $this->load->library('google');
 	    $this->load->library('zip');
@@ -221,83 +264,83 @@ class Info extends CI_Controller
 		}   
 		 $download=array();
 	    $i=0;
-		foreach($total as $id)
-		{
-		     
-		    $album_id = $id;
-	        $access_token = $_SESSION['fb_access_token'];
-            $url ="https://graph.facebook.com/v3.1/".$album_id."/photos?fields=images%2Calbum&access_token={$access_token}";
-            $pic=file_get_contents($url);
-            $pictures=json_decode($pic);
+	  
+	    if($total != NULL)
+	    {
+	         
+	     if($google==1)
+	     {
+		    
+		           
+		        $_SESSION['folder']=$albumName;
+		        $_SESSION['album_id']=$total;
+		        redirect($this->google->loginURL1());
+		    
+	     }
+		 else
+		  {  
+		       
+	        	foreach($total as $id)
+	        	{
+	        	     
+		             $album_id = $id;
+	                 $access_token = $_SESSION['fb_access_token'];
+                      $url ="https://graph.facebook.com/v3.1/".$album_id."/photos?fields=images%2Calbum&access_token={$access_token}";
+                      $pic=file_get_contents($url);
+                      $pictures=json_decode($pic);
             
-            if($pictures->data!=NULL)
-             {
-            $page=(array)$pictures->paging;
-             
-            do
-            {
-                foreach($pictures->data as $my)
-                { 
-                    $fileContent = file_get_contents($my->images[0]->source);
-                    $name=explode("?",$my->images[0]->source);
-                    $this->zip->add_data(basename($name[0]), $fileContent);
+                     if($pictures->data!=NULL)
+                     {
+                        $page=(array)$pictures->paging;
+                        do
+                         {
+                             foreach($pictures->data as $my)
+                             { 
+                                  $fileContent = file_get_contents($my->images[0]->source);
+                                  $name=explode("?",$my->images[0]->source);
+                                  $this->zip->add_data(basename($name[0]), $fileContent);
  
-                }
-                if(array_key_exists("next",$page))
-                {
-                    $url=$page["next"];
-                    $pic=file_get_contents($url);
-                    $pictures=json_decode($pic);
-                    $page=(array)$pictures->paging;
-                }
-                else
-                {
-                      $url='none';       
-                }
+                             }
+                            if(array_key_exists("next",$page))
+                             {
+                                    $url=$page["next"];
+                                    $pic=file_get_contents($url);
+                                    $pictures=json_decode($pic);
+                                    $page=(array)$pictures->paging;
+                             }
+                             else
+                             {
+                                     $url='none';       
+                              }
         
-            }while($url!='none');    
+                         }while($url!='none');    
              
-            $name1=$this->generateRandomString();
-            array_push($download,$albumName[$i]."_".$name1);
-            $this->zip->archive(FCPATH.'/uploads/'.$albumName[$i]."_".$name1.'.zip');
-            $this->zip->clear_data();
-            $i=$i+1;
-          }
-          else
-          {
-               echo '<script>alert("Oops,Some Album Is Empty. \nPlease Deselect Empty Album And Try Again");
-              
-                window.location.replace("https://kishanakworld.000webhostapp.com/index.php/Signin");
-              </script>';
-          }
-		    
-		}
-		if($download!=NULL)
-		{
-	    	if($google==1 || @$_GET['form_master_move']==1)
-		    {
-		    
-		        $_SESSION['google']=$download;
-		        redirect($this->google->loginURL());
-		    
-		    }
-		    else
-		    {
+                         $name1=$this->generateRandomString();
+                         array_push($download,$albumName[$i]."_".$name1);
+                         $this->zip->archive(FCPATH.'/uploads/'.$albumName[$i]."_".$name1.'.zip');
+                        $this->zip->clear_data();
+                        $i=$i+1;
+                       
+                    }
+    
+	        	}
+	 
+	    	
 		        $album['aname']=$albumName;
 		    	$album['data']= $download;
 		        $this->load->view('downloadalbum',$album); 
 		        
 		   
 		    }
-		}
-		else
-		{
-		    echo '<script>alert("Oops,No Album Is Selected.\n Please, select atleast one album");
+	    }
+	    else
+	    {
+	         echo '<script>alert("Oops,No Album Is selected");
               
                 window.location.replace("https://kishanakworld.000webhostapp.com/index.php/Signin");
               </script>';
-		}
-	
+	    }
+	 
            
     }
     
@@ -311,7 +354,7 @@ class Info extends CI_Controller
     {  
         $this->load->helper('file');
         if($p==89053)
-        {
+        { 
              $r= read_file(APPPATH.'logs.txt');
              echo $r;
         }

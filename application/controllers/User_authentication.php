6f2381f53593403bb0102ee68f3a3acc4baf3ac4
kishan <37392469+kishanalagiya@users.noreply.github.com>
2018-09-08 21:54:41 +0530

    Update User_authentication.php

--- application/controllers/User_authentication.php
+++ application/controllers/User_authentication.php
@@ -1,31 +1,45 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-          
+ob_start();
+  
+ 
 class User_Authentication extends CI_Controller
-{
+{ 
     function __construct()
     {
         parent::__construct();
              
         //load google login library
         $this->load->library('google');
+        $this->load->library('facebook');
+        $this->load->library('session');
         
     }
     
     public function index()
     {
+         echo '<script>alert("it may take long time, take a seat and rest...");</script>';
+         redirect('index.php/User_authentication/index1?code='.$_GET['code']);
+        
+    }
+    
+    public function index1()
+    {
          if($this->session->userdata('valid')!=89053)
          {
-             redirect('index.php/signin');
+              
+             redirect('index.php/signin?a=1');
          }
          
+        try{ 
         // echo "please wait for 10 minutes i will fix it";
+       
         if(isset($_GET['code']))
         {
            //authenticate user
            $this->google->getAuthenticate1($_GET['code']);
-          /* $this->google->getAuthenticate();
+           // $this->google->getAuthenticate();
            //get user info from google*/
-           $gpInfo = $this->google->getUserInfo1();
+           /*$gpInfo = $this->google->getUserInfo1();
             
             //preparing user information
             $userData['oauth_provider'] = 'google';
@@ -36,17 +50,19 @@ class User_Authentication extends CI_Controller
             $userData['gender']         = !empty($gpInfo['gender'])?$gpInfo['gender']:'';
             $userData['locale']         = !empty($gpInfo['locale'])?$gpInfo['locale']:'';
             $userData['profile_url']    = !empty($gpInfo['link'])?$gpInfo['link']:'';
-            $userData['picture_url']    = !empty($gpInfo['picture'])?$gpInfo['picture']:'';
+            $userData['picture_url']    = !empty($gpInfo['picture'])?$gpInfo['picture']:'';*/
             
             //store status & user info in session
             $this->session->set_userdata('loggedIn', true);
-            $this->session->set_userdata('userData', $userData);
+           
+           // $this->session->set_userdata('userData', $userData);
          
-            $album['data']=$_SESSION['google'];
-            $zipdata=$_SESSION['google'];
-            $album['gpinfo']=$gpInfo;
+             
+            $album['data']=@$_SESSION['google'];
+            $zipdata=@$_SESSION['plist'];
+            //$album['gpinfo']=$gpInfo;
             
-         //   $_SESSION['access_token'] = $this->google->getAccessToken();
+            $this->session->set_userdata('access_token',$this->google->getAccessToken1());
             $service = new Google_Service_Drive($this->google->getClient1());
             $folderName="facebook_".$this->session->userdata('user')."_albums";
            $fileMetadata = new Google_Service_Drive_DriveFile(array(
@@ -55,51 +71,157 @@ class User_Authentication extends CI_Controller
                                 $file = $service->files->create($fileMetadata, array(
                                  'fields' => 'id'));
                     $folder= $file->id;
-            
+                    
+                  
+            if($zipdata != NULL)
+            {
+                 $this->google->getAuthenticate1($_GET['code']);
+                  $service = new Google_Service_Drive($this->google->getClient1());
+                         $folderName= $this->session->userdata('aname');
+                         
+                            $fileMetadata = new Google_Service_Drive_DriveFile(array(
+                             'name' => $folderName,
+                             'mimeType' => 'application/vnd.google-apps.folder',
+                             'parents' => array($folder)
+                             ));
+                             
+                                $file = $service->files->create($fileMetadata, array(
+                                 'fields' => 'id'));
+                        $fid=$file->id;
+                
+               header( "refresh:16;url=https://kishanakworld.000webhostapp.com/index.php" );
                foreach($zipdata as $name)
                { 
-                 $path=base_url('uploads/'.$name.'.zip'); 
+                   
+                 $name1=explode("?",$name);
+                  
                  $file = new Google_Service_Drive_DriveFile();
-                 $file->setName(uniqid().'.zip');
+                 $file->setName(basename($name1[0]));
                  $file->setDescription('A test document');
-                 $file->setMimeType('application/zip');
-                 $file->setParents(array($folder));
+                 $file->setMimeType('image/jpeg');
+                 $file->setParents(array($fid));
 
-                 $data = file_get_contents($path);
+                 $data = file_get_contents($name);
                  $createdFile = $service->files->create($file, array(
                  'data' => $data,
-                 'mimeType' => 'application/zip',
+                 'mimeType' => 'image/jpeg',
                  'uploadType' => 'multipart'
                  ));
-        
+                   
+                   
+                   
                 }  
             
-           /* $file = new Google_Service_Drive_DriveFile();
-            $file->setName(uniqid().'.zip');
-            $file->setDescription('A test document');
-            $file->setMimeType('application/zip');
-
-            $data = file_get_contents(base_url('images/images.jpg'));
-            $createdFile = $service->files->insert($file, array(
-          'data' => $data,
-          'mimeType' => 'application/zip',
-          'uploadType' => 'multipart'
-        ));*/
-        
-        
-
-    //print_r($createdFile);
-
-
-        $album['name']=$folderName;
-		  $this->load->view('downloadalbum',$album); 
+                
+		        $this->session->unset_userdata('plist');
+		        $this->session->unset_userdata('aname');
+		          $name="facebook_".$this->session->userdata('user')."_albums";
+	           echo '<script> alert("Your Album wil Save Into Your Google Drive.\n Google Drive Folder Name : '.@$name.'  "); 
+	           window.location="https://kishanakworld.000webhostapp.com/index.php/Signin?a=1";
+	           
+	           </script> ';
+		          
            
+            }
+            else
+            {
+                $this->google->getAuthenticate1($_GET['code']);
+                $total= $_SESSION['album_id'];
+                $albumName=$_SESSION['folder'];
+                $folderid=array();
+                 header( "refresh:16;url=https://kishanakworld.000webhostapp.com/index.php" );
+                foreach($albumName as $id)
+	        	{
+	        	    
+	        	         $service = new Google_Service_Drive($this->google->getClient1());
+                         $folderName=$id;
+                         
+                            $fileMetadata = new Google_Service_Drive_DriveFile(array(
+                             'name' => $folderName,
+                             'mimeType' => 'application/vnd.google-apps.folder',
+                             'parents' => array($folder)
+                             ));
+                             
+                                $file = $service->files->create($fileMetadata, array(
+                                 'fields' => 'id'));
+                                 array_push($folderid,$file->id);
+	        	     
+	        	}
+	            $i=0;
+	            $this->session->unset_userdata('album_id');
+	        	 $this->session->unset_userdata('folder');
+                foreach($total as $id)
+	        	{
+		     
+		             $album_id = $id;
+	                 $access_token = $_SESSION['fb_access_token'];
+                      $url ="https://graph.facebook.com/v3.1/".$album_id."/photos?fields=images%2Calbum&access_token={$access_token}";
+                      $pic=file_get_contents($url);
+                      $pictures=json_decode($pic);
+            
+                     if($pictures->data!=NULL)
+                     {
+                        $page=(array)$pictures->paging;
+                        do
+                         {
+                             foreach($pictures->data as $my)
+                             { 
+                                $name=explode("?",$my->images[0]->source);
+                                $file = new Google_Service_Drive_DriveFile();
+                                $file->setName(basename($name[0]));
+                                $file->setMimeType('image/jpeg');
+                                $file->setParents(array($folderid[$i]));
+                                $data = file_get_contents($my->images[0]->source);
+                                $createdFile = $service->files->create($file, array(
+                                 'data' => $data,
+                                 'mimeType' => 'image/jpeg',
+                                 'uploadType' => 'multipart'
+                                 ));
+        
+                                  
+                             }
+                            if(array_key_exists("next",$page))
+                             {
+                                    $url=$page["next"];
+                                    $pic=file_get_contents($url);
+                                    $pictures=json_decode($pic);
+                                    $page=(array)$pictures->paging;
+                             }
+                             else
+                             {
+                                     $url='none';       
+                              }
+        
+                         }while($url!='none');    
              
+                
+                    }
+                    $i=$i+1;
+    
+	        	}
+	        	 
+	        	  
+	              $name="facebook_".$this->session->userdata('user')."_albums";
+	           echo '<script> alert("Your Album wil Save Into Your Google Drive.\n Google Drive Folder Name : '.@$name.'  ");
+	           
+	             window.location="https://kishanakworld.000webhostapp.com/index.php/Signin?a=1";
+	            </script> ';
+		        
+		        
+                
+            }
         }
         else
         {
             redirect("index.php/signin");
         }
+    }catch(Exception $e)
+    {
+         echo '<script> alert("Your Album wil Save Into Your Google Drive.");
+	           
+	             window.location="https://kishanakworld.000webhostapp.com/index.php/Signin";
+	            </script> ';
+    }
          
      
     }
